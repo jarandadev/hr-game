f5b76ba615789ceaf60a5ccf8cb4dd2b60a08c4a
1704682@uab.cat <jarandadev@gmail.com>
2024-03-08 14:37:08 +0100

    Fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,10 +2,14 @@ import { SOCKET_PORT } from "@/config/app"
 import { io } from "socket.io-client"
 
 export default function socketClient() {
-  const socket = io(`:${SOCKET_PORT}`, { path: "/api/socket", addTrailingSlash: false })
+  const socket = io(`:${SOCKET_PORT}`, { 
+    path: "/api/socket", 
+    addTrailingSlash: false,
+    transports: ["websocket"],
+   })
 
-  socket.on("connect_error", async () => {
-    await fetch("/api/socket")
+  socket.on("connect_error", async (error) => {
+    console.error("Socket connection error", error)
   })
 
   return socket
